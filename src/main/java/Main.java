85d8366f9c31d12546acae88426fe39ceee9b678
john <jonhmiguel138@gmail.com>
2026-03-29 12:16:51 -0300

    feat: extract methods createTask, listTasks and setCompletedTask

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,59 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static String[] titles = new String[50];
+    static String[] priorities = new String[50];
+    static boolean[] completed = new boolean[50];
+    static int taskCount = 0;
+    public static void createTask(){
+        System.out.println("Title: ");
+        String title = sc.nextLine();
+        while (title.isBlank()){
+            System.out.println("Please, type a title for the task: ");
+            title = sc.nextLine();
+        }
+        System.out.println("Priority(High/Medium/Low)");
+        String priority = sc.nextLine();
+        titles[taskCount] = title;
+        priorities[taskCount] = priority;
+        completed[taskCount] = false;
+        taskCount++;
+    }
+
+    public static void listTasks(){
+        for (int i = 0; i < taskCount; i++) {
+            String status = completed[i] ? "Completed" : "Pending";
+            System.out.println("Task -> Title: " + titles[i] + " | Priority: " + priorities[i] + " | Status: " + status);
+        }
+    }
+
+    public static void setCompletedTask(){
+        System.out.println("Give the title of task: ");
+        String titleTaskSearch = sc.nextLine();
+        boolean taskFounded = false;
+        for (int task = 0; task < taskCount; task++) {
+            if (titleTaskSearch.equals(titles[task])){
+                completed[task] = true;
+                taskFounded = true;
+            }
+        }
+        if (!taskFounded){
+            System.out.println("Task not found! Please try again!");
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] titles = new String[50];
-        String[] priorities = new String[50];
-        boolean[] completed = new boolean[50];
-        int taskCount = 0;
         while (true){
-            System.out.println(" [1] - Create task\n [2] - List tasks\n [0] - Exit\n Choice: ");
+            System.out.println(" [1] - Create task\n [2] - List tasks\n [3] - Mark task as completed \n [0] - Exit\n Choice: ");
             int choice = Integer.parseInt(sc.nextLine());
             if (choice == 1){
-                System.out.println("Title: ");
-                String title = sc.nextLine();
-                while (title.isBlank()){
-                    System.out.println("Please, type a title for the task: ");
-                    title = sc.nextLine();
-                }
-                System.out.println("Priority(High/Medium/Low)");
-                String priority = sc.nextLine();
-                titles[taskCount] = title;
-                priorities[taskCount] = priority;
-                completed[taskCount] = false;
-                System.out.println("Task created!");
-                taskCount++;
+                createTask();
+                System.out.println("Task Created!");
             } else if (choice == 2) {
-                for (int i = 0; i < taskCount; i++) {
-                    String status = completed[i] ? "Completed" : "Pending";
-                    System.out.println("Task -> Title: " + titles[i] + " | Priority: " + priorities[i] + " | Status: " + status);
-                }
+                listTasks();
+            } else if (choice == 3) {
+                setCompletedTask();
             } else if (choice == 0) {
                 System.out.println("Leaving...");
                 break;
