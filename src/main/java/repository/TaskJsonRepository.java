817c38631949107629de7d0b37ccd2d29ceb54b9
john <jonhmiguel138@gmail.com>
2026-07-07 22:29:10 -0300

    feat(repository): implementation of TaskJsonRepository

---
+++ src/main/java/repository/TaskJsonRepository.java
@@ -0,0 +1,47 @@
+package repository;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import model.Task;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+
+public class TaskJsonRepository {
+
+    private static final String FILE_PATH = "tasks.json";
+    private Gson gson;
+
+    public TaskJsonRepository(){
+        this.gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, (JsonSerializer<LocalDate>) (src, typeOfSrc, context) -> new JsonPrimitive(src.toString()))
+                .registerTypeAdapter(LocalDate.class, (JsonDeserializer<LocalDate>) (json, typeOfT, context) -> LocalDate.parse(json.getAsString()))
+                .setPrettyPrinting()
+                .create();
+    }
+
+    public void saveTasks(List<Task> tasks) {
+        try (Writer writer = new FileWriter(FILE_PATH)){
+
+        } catch (IOException ioException){
+            System.out.println("Erro at store tasks: " + ioException.getMessage());
+        }
+    }
+
+    public List<Task> loadTasks() {
+        File file = new File(FILE_PATH);
+        if (!file.exists()){
+            return new ArrayList<>();
+        }
+        try (Reader reader = new FileReader(FILE_PATH)) {
+            Type listType = new TypeToken<ArrayList<Task>>(){}.getType();
+            return gson.fromJson(reader, listType);
+        } catch (IOException e) {
+            System.out.println("Error at load as tasks: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+}
